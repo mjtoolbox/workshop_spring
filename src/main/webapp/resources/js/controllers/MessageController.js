ba33c8c4ac4136208e4e71bef8b983da9f1f7090
mijo <michael.jo@bchydro.com>
2016-07-07 14:26:57 -0700

    Minor update pom.xml

--- src/main/webapp/resources/js/controllers/MessageController.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by mijo on 2015-06-23.
- */
-angular.module('myApp')
-.controller('MessageController', ['$scope', '$http', function ($scope, $http) {
-        $http.get('spring/service/messages').success(function (data) {
-            $scope.messageEvents = data;
-        });
-}]);
